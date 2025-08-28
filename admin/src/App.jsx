15f304c9920439d09dbc5259e2a96591bb311b9b
SaajidMohammed <saajidmohammed6@gmail.com>
2025-08-29 00:22:06 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css'
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://tomato-app-backend-tttw.onrender.com"
 
   return (
     <div>
