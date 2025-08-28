2e9b1c341e1cbe9e08cf767bb5698003ed520cba
SaajidMohammed <saajidmohammed6@gmail.com>
2025-08-29 01:44:23 +0530

    Update orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -101,4 +101,14 @@ const updateStatus = async (req,res) => {
     }
 }
 
-export { placeOrder, userOrder, verifyOrder, listOrders, updateStatus  };
+const myOrders = async (req,res) => {
+    try {
+        const orders = await orderModel.find({userId:req.body.userId});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
+export { placeOrder, userOrder, verifyOrder, listOrders, updateStatus, myOrders  };
